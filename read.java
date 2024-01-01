12b2442e65a9d44085ae038704e6b3c8e64b2a3f
Ashi20037 <120173059+Ashi20037@users.noreply.github.com>
2024-01-01 22:27:37 +0530

    Create read.java

---
+++ read.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class read{
+  public static void main(String args[]){
+    Scanner s=new Scanner(System.in);
+    int n;
+    n=s.nextInt();
+    System.out.println("The n value is"+n);
+  }
+}
